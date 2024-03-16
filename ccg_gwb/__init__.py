800926eb2d41400587ab16acd1f9a85dcef99f0f
Mohammad Alakhras <mohammadalakhras1989@gmail.com>
2024-03-17 02:33:34 +0330

    make cache directory

--- ccg_gwb/__init__.py
+++ ccg_gwb/__init__.py
@@ -10,6 +10,9 @@ CCG_PATH = sys.exec_prefix + "/bin"
 CCG_ENV["PATH"] = CCG_PATH + ":" + CCG_ENV["PATH"]
 CCG_CACHEDIR = user_cache_dir(appname="ccg_gwb", appauthor=False)
 
+if not os.path.exists(CCG_CACHEDIR):
+    os.mkdir(CCG_CACHEDIR)
+
 __author__ = """Mohammad Alakhras"""
 __email__ = "mohammadalakhras1989@gmail.com"
 __version__ = "0.1.1"
