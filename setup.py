a840fd57525e16e232c1f2df891ebd1d40466954
Mohammad Alakhras <mohammadalakhras1989@gmail.com>
2024-03-17 02:20:58 +0330

    add requirement to setup.py

--- setup.py
+++ setup.py
@@ -8,39 +8,46 @@ with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 
-requirements = []
-
-test_requirements = [
-    "pytest>=3",
+requirements = [
+    "appdirs>=1.4.4",
+    "astropy>=6.0.0",
+    "pint-pulsar>=0.9.8",
+    "psrqpy>=1.2.8",
+    "tqdm>=4.66.2",
 ]
 
+test_requirements = []
+
 setup(
-    author="Mohammad Alakhras",
-    author_email="mohammadalakhras1989@gmail.com",
-    python_requires=">=3.6",
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-    ],
+    name="ccg_gwb",
     description="A package developed by Complex & Cosmology Group (CCG) at Shahid Beheshti University (SBU). \
     It uses data-driven techniques to search the pulsar timing arrays (PTAs) data for \
     gravitational wave background (GWB) signal.",
+    long_description=readme,
+    author="Mohammad Alakhras",
+    author_email="mohammadalakhras1989@gmail.com",
+    url="https://github.com/M-Alakhras/ccg_gwb",
+    packages=find_packages(include=["ccg_gwb", "ccg_gwb.*"]),
+    package_dir={"ccg_gwb": "ccg_gwb"},
+    python_requires=">=3.9, <=3.11",
     install_requires=requirements,
     license="MIT license",
-    long_description=readme,
-    include_package_data=True,
+    zip_safe=False,
     keywords="ccg_gwb",
-    name="ccg_gwb",
-    packages=find_packages(include=["ccg_gwb", "ccg_gwb.*"]),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Scientific/Engineering :: Astronomy",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    include_package_data=True,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/M-Alakhras/ccg_gwb",
-    version="0.1.1",
-    zip_safe=False,
 )
