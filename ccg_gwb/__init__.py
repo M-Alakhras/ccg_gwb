909d90320677e476bdfa2be62d0e73da5397ee12
Mohammad Alakhras <mohammadalakhras1989@gmail.com>
2024-04-05 06:18:05 +0330

    add driver class for ccg_gwb functions.
    add CCG_Session() class which is a driver for other ccg_gwb functions.
    add print_all_sessions(), delete_all_sessions(), __init__(), load(), save(), delete() methods.
    update docstring to ccg_gwb/__init__.py
    add logging functionality.

--- ccg_gwb/__init__.py
+++ ccg_gwb/__init__.py
@@ -1,14 +1,54 @@
-"""Top-level package for ccg_gwb."""
+"""  *                 *                        *          *
+   *             *           *           *
+*           ##########################################    *      *
+       *    #========================================#
+            #||   ======     ======     ======     ||#
+   *        #||  //         //         //          ||#            *
+            #|| //         //         //           ||#    *
+      *     #|| ||         ||         ||           ||#
+            #|| ||         ||         ||           ||#       *
+*           #|| ||         ||         ||     ====  ||#   *
+      *     #|| \\\         \\\         \\\      ||   ||#
+   *        #||  \\\         \\\         \\\     //   ||#       *
+            #||   ======     ======     ======/    ||#
+            #========================================#    *
+      *     ########################################## *
+            *            *             *          *            *
+    *          *             *   *          *           *
+CCG_GWB: Gravitational Wave Background analysis tools.
+A package developed by Complex & Cosmology Group (CCG) at Shahid Beheshti University (SBU).
+it uses data-driven techniques to search the pulsar timing arrays (PTAs) data for gravitational
+wave background (GWB) signal.
 
+Usage:
+------
+from ccg_gwb.ccg_gwb import CCG_Session
+
+# create a new session:
+my_session = CCG_Session()
+# load a previous session:
+my_session = CCG_Session.load(session_name)
+# save a session:
+my_session.save()
+# delete a session:
+CCG_Session.delete()
+"""
+
+import glob
 import os
 import sys
 
 from appdirs import user_cache_dir
+from loguru import logger as log
 
 CCG_ENV = os.environ.copy()
 CCG_PATH = sys.exec_prefix + "/bin"
 CCG_ENV["PATH"] = CCG_PATH + ":" + CCG_ENV["PATH"]
 CCG_CACHEDIR = user_cache_dir(appname="ccg_gwb", appauthor=False)
+
+format = "<level>{level: <8}</level> ({name: <30}): <level>{message}</level>"
+log.remove(0)
+log.add(sys.stderr, level="DEBUG", format=format)
 
 if not os.path.exists(CCG_CACHEDIR):
     os.mkdir(CCG_CACHEDIR)
@@ -16,3 +56,14 @@ if not os.path.exists(CCG_CACHEDIR):
 __author__ = """Mohammad Alakhras"""
 __email__ = "mohammadalakhras1989@gmail.com"
 __version__ = "0.1.1"
+
+available_sessions = glob.glob(CCG_CACHEDIR + "/*.session")
+
+print(__doc__)
+if len(available_sessions) > 0:
+    print("Previeous sessions: ")
+    for i, session in enumerate(available_sessions):
+        session_name = os.path.basename(session).split(".")[0]
+        print(f"   {i+1}- {session_name}")
+else:
+    print("No previeous sessions.")
